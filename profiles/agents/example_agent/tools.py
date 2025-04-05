0348f65e24ada7b1016d1ecb925497d4af9899a8
Chris Kroenke <christopher.kroenke@gmail.com>
2025-04-04 20:30:03 -0400

    changes

--- profiles/agents/example_agent/tools.py
+++ profiles/agents/example_agent/tools.py
@@ -10,5 +10,13 @@ def get_current_time() -> str:
     print(f"Tool Execution: get_current_time() -> {now}") # note: this prints in the chat session
     return now
 
+@tool
+def think(
+    thought: str, # the thought or idea to think about to solve the user request
+    ) -> str: # do not worry about returning anything, only think
+    """Use this tool to think about the user's request. You will not obtain new information or change the user's input. It only helps you think about the task and problem to maximally, accurately, and completely solve the user's request."""
+    print(f"Tool Execution: think() -> {thought}")
+    return thought
+
 # group up the tools for easy import
-tools = [get_current_time]
+tools = [get_current_time, think]
